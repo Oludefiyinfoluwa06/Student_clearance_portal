6f90593870f4c3e2792faa8cfeb2383892d8c12f
Timothy <oludefiyinfoluwa06@gmail.com>
2023-11-03 13:02:18 +0100

    Update

--- login.php
+++ login.php
@@ -55,6 +55,7 @@
             margin: 0;
             padding: 0;
             box-sizing: border-box;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
         }
 
         .body {
